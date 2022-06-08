0ac75c2f748c065f366acd7f5845ddae39253756
Zacharia <ndunguzachary24@gmail.com>
2022-06-08 21:03:07 +0300

    commit tree 24863419daa0991eb3a24d97c2e4b3020fa0ebf5

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = (num, num2) =>{
+  num= 2000
+ num2 = 100
+ return num/num2
+}
+const square = (x) =>{
+    x = 2
+    return x **x
+}
+const add =(par, para) =>{
+par= 3
+para = 4
+return par + para
+}
